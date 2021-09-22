b4c63e2a348d22cd77b8e488b7f8f082d0c2972e
unknown <wellingthon741@gmail.com>
2021-09-22 10:47:52 -0300

    Includes from tables databases

--- database/migrations/2021_09_22_134140_create_tb_programas_table.php
+++ database/migrations/2021_09_22_134140_create_tb_programas_table.php
@@ -15,7 +15,8 @@ class CreateTbProgramasTable extends Migration
     {
         Schema::create('tb_programas', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('nome', 30);
+            $table->string('descricao', 200);
         });
     }
 
